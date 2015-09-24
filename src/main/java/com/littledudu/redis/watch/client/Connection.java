e244b842d419ce842017985133b89c22c08f1fcf
Jinjun Hu <hujj0615@163.com>
2015-09-24 13:48:04 +0800

    initial commit

---
+++ src/main/java/com/littledudu/redis/watch/client/Connection.java
@@ -0,0 +1,13 @@
+package com.littledudu.redis.watch.client;
+
+public class Connection {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
